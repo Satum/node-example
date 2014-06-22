96aac665eb6c1e0073e29584ebc1cafe302a8c31
Oliver Owens <oli.owens@foo.com>
2014-06-22 17:01:40 +1000

    procfile fix

--- web.js
+++ web.js
@@ -5,16 +5,23 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-var i, a, b, c, max;
+app.get('/', function(req, res) {
+  var i, a, b, c, max;
  
-max = 1000000000;
- 
-var d = Date.now();
- 
-for (i = 0; i < max; i++) {
-    a = 1234 + 5678 + i;
-    b = 1234 * 5678 + i;
-    c = 1234 / 2 + i;
-}
+	max = 1000000000;
+	 
+	var d = Date.now();
+	 
+	for (i = 0; i < max; i++) {
+	    a = 1234 + 5678 + i;
+	    b = 1234 * 5678 + i;
+	    c = 1234 / 2 + i;
+	}
  
 console.log(Date.now() - d);
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
