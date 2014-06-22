d6d4c0314c11f514cfc9eae796ac3a52b59900ab
Oliver Owens <oli.owens@foo.com>
2014-06-22 16:59:16 +1000

    Commit date counter

--- web.js
+++ web.js
@@ -5,11 +5,16 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var i, a, b, c, max;
+ 
+max = 1000000000;
+ 
+var d = Date.now();
+ 
+for (i = 0; i < max; i++) {
+    a = 1234 + 5678 + i;
+    b = 1234 * 5678 + i;
+    c = 1234 / 2 + i;
+}
+ 
+console.log(Date.now() - d);
